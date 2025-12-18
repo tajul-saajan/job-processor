89b7a47e987931d70529cf336931a3944f59a875
Tajul Islam <tajul.islam@wsd.com>
2025-12-19 00:44:27 +0600

    feat(web): :zap: added new routes to test prefix, app data

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,35 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::resource("/test").route(web::route().to(test)));
+}
+
+async fn test() -> impl Responder {
+    HttpResponse::Gone().body("in test")
+}
+
+struct AppState {
+    app_name: String,
+}
+
+#[get("/state")]
+async fn get_state(data: web::Data<AppState>) -> impl Responder {
+    let app_name = &data.app_name;
+    format!("{}", app_name)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .app_data(web::Data::new(AppState {
+                app_name: "my_app".into(),
+            }))
+            .configure(config)
             .service(hello)
             .service(echo)
+            .service(get_state)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
