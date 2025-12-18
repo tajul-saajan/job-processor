56c017451bb66a4cccbf5e2a9ef7a48e92a704a3
Tajul Islam <tajul.islam@wsd.com>
2025-12-19 01:21:58 +0600

    feat(web): :sparkles: moved dummy routes to a new module

---
+++ src/http/dummy.rs
@@ -0,0 +1,22 @@
+use actix_web::{HttpResponse, Responder, get, post, web};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+pub fn get_scope() ->actix_web::Scope {
+    web::scope("dummy")
+        .service(hello)
+        .service(echo)
+        .route("/hey", web::get().to(manual_hello))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,6 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
+mod http;
+use http::dummy::get_scope as get_dummy_scope;
 
 fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(web::resource("/test").route(web::route().to(test)));
@@ -40,10 +28,8 @@ async fn main() -> std::io::Result<()> {
                 app_name: "my_app".into(),
             }))
             .configure(config)
-            .service(hello)
-            .service(echo)
+            .service(get_dummy_scope())
             .service(get_state)
-            .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
