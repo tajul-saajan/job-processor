0593f3b497d45ebe9f35ba522fa7e698622a2978
Tajul Islam <tajul.islam@wsd.com>
2025-12-19 21:43:13 +0600

    feat(web): :lock: tested guard concept

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use actix_web::{App, HttpResponse, HttpServer, Responder, guard, web};
 mod http;
 use http::dummy::get_scope as get_dummy_scope;
 use http::state::get_scope as get_state_scope;
@@ -15,13 +15,24 @@ async fn test() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    
     HttpServer::new(|| {
         let my_state = web::Data::new(AppState::new("my_app"));
-        App::new().app_data(my_state)
+        App::new()
+            .app_data(my_state)
             .configure(config)
             .service(get_state_scope())
             .service(get_dummy_scope())
+            .service(
+                web::scope("/guard")
+                    .guard(guard::Host("www.tajul.com"))
+                    .route("", web::to(|| async { HttpResponse::Ok().body("tajul") })),
+            )
+            .service(
+                web::scope("/guard")
+                    .guard(guard::Host("www.saajan.com"))
+                    .route("", web::to(|| async { HttpResponse::Ok().body("saajan") })),
+            )
+            .route("/guard", web::to(HttpResponse::Ok))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
